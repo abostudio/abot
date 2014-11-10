798a76c060a2a815383be560c6b2ffc0b2124661
yangxbme <yangxbme@gmail.com>
2014-11-10 10:18:34 +0800

    check in

---
+++ modules/foundation/ScriptEngine.h
@@ -0,0 +1,26 @@
+#ifndef ABOT_FOUNDATION_SCRIPTENGINE_H
+#define ABOT_FOUNDATION_SCRIPTENGINE_H
+
+#include "Abot/Foundation.h"
+
+namespace Abot {
+
+	
+	class ScriptEngine : public std::enable_shared_from_this <ScriptEngine> {
+
+	private:
+
+	public:
+
+
+	public:
+		void Run(const std::string & script) {
+
+		}
+	
+
+	};
+}
+
+
+#endif //ABOT_FOUNDATION_SCRIPTENGINE_H
